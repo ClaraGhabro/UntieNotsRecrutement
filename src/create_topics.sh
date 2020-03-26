6c98ec01c594faba5a2d9f30deb425913f3fdddc
Clara Ghabro <clara.ghabro@epita.fr>
2020-03-26 18:10:17 +0100

    topics: script to create needed kafka topics

--- src/create_topics.sh
+++ src/create_topics.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic sendWord
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic topicKeyword
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic topicName
+KAFKA_HOME=/home/clara/Outils/kafka_2.13-2.4.1
+
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic sendWord
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic topicKeyword
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic topicName
